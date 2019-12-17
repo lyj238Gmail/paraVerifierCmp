030b7f52a6336208d4d51de3fc751f53d54eb8af
apple <apple@appledeiMac.local>
2019-12-17 18:12:50 +0800

    kk

--- examples/src/genCmpProof.ml
+++ examples/src/genCmpProof.ml
@@ -97,7 +97,7 @@ let lemmaProofGenProps rulePds props result=
   |x:xs ->lemmaProofGenProps rulePds xs 
     (lemmaProofGen rulePds prop result)
 
- let lemmaPooofGen (invName,paraNum) count=
+ (*let lemmaPooofGen (invName,paraNum) count=
  let invName01=if (paraNum=0) then invName else if(paraNum=1) then (invName^"0") else (invName^"0 1") in
  let invNamei1= if (paraNum=0) then invName else if(paraNum=1) then (invName^"i") else (invName^"i 0") in
  let invNamei0=if (paraNum=0) then invName else if(paraNum=1) then (invName^"i") else (invName^"0 1") in
@@ -121,26 +121,66 @@ let lemmaProofGenProps rulePds props result=
     let lenList=List.map  ~f:(fun i->4*i)  (up_to (List.length invNameList) )   in
     let m=List.zip invNameList lenList in 
     let str1=String.concat ~sep:"\n" (List.map ~f:(fun (invName,count)-> lemmaPooofGen invName count) ) in
-    str1
+    str1*)
 
-let genPart1 invNameList=
-    let str1=lemmaProofGens invNameList in
-    let strs=List.map ~f:(fun i-> "c"^(itoa i)) m in
+let genPart1 r props1 proofStr=
+    let (rn,pds, pr)=r in
+    let (str1,count)=lemmaProofGenProps pds props1 (proofStr,3) in
+    
+    let strs=List.map ~f:(fun i-> "c"^(itoa i)) (up_to m) in
     let str2=String.concat ~sep:" " strs in
-    let end1=sprintf "
+    let end1=sprintf "%s
       from b1 %s show \"formEval (pre ?r') s\" 
        by auto
-   qed
-   next     
-   show \"(∀  v. v ∈  varOfSent (act  ?r') ⟶  v ∈ varOfFormList ?F ⟶ formEval (pre ?r) s ⟶ 
-    expEval (substExpByStatement (IVar v)  (act ?r')) s = expEval (substExpByStatement (IVar v)  (act ?r)) s)\"
-   proof(rule allI,(rule impI)+)
-      fix  v
-     assume b1:\"v∈ (varOfFormList invariantsAbs)\"  and b2:\"formEval (pre ?r) s\" and b3:\"v ∈varOfSent (act ?r')\"" str2 in
-   str1^end1
+     qed
+   next 
+    show \"∀v. v∈varOfForm (pre ?r') ⟶ v ∈(varOfFormList invariantsAbs)\"
+      by auto"  in 
+      str1
+      str2  in
+    end1
+    
 
-let genPart2 invNameList=
-    let str1=lemmaProofGens invNameList in
+let genPart2 props2 proofStr=
+
+  let proofStr1=
+  match props2 with
+  |[]->""
+  |[prop]->
+  let [pn,ppds,pinv]=prop in
+        sprintf 
+        "from a4  have tmp:\"formEval (%s 0  ) s\"   by (force simp del:%s_def)
+     have b4:\"formEval (%s i  ) s\" 
+     proof(cut_tac a1 a2 a3 tmp,rule axiomOnf1,force+)qed"
+          pn pn
+          pn
+  in 
+  sprintf "%s
+  %s
+  show "expEval (substExpByStatement (IVar v)  (act ?r')) s = expEval (substExpByStatement (IVar v)  (act ?r)) s"  
+       apply (cut_tac  a1 b1 b2 b4,auto) done
+   qed
+ next
+   show "∀  v. v ∈  varOfSent (act ?r) ⟶  v ∈ varOfFormList ?F ⟶ v ∈  varOfSent (act ?r')" by(cut_tac a1, auto)
+
+  
+ next 
+   show "∀ v va. v ∈ varOfSent (act ?r') ⟶va∈varOfExp ( substExpByStatement (IVar v)  (act ?r'))⟶ va ∈ (varOfFormList ?F)"
+      by auto  
+qed"     
+  proofStr
+  proofStr1
+
+    
+
+let lemmaProofOnGt props1 props2 r rAbs=
+    let head=lemmaHeadGen  r rAbs in
+    let part1= genPart1 r props1 head in
+    let part2=genPart2 props2 part1 in
+    part2
+
+
+(*let str1=lemmaProofGens invNameList in
     let strs=List.map ~f:(fun i-> "c"^(itoa i)) (List.length invNameList) in
     let str2=String.concat ~sep:" " strs in
     let end1=sprintf "show \"expEval (substExpByStatement (IVar v)  (act ?r')) s = expEval (substExpByStatement (IVar v)  (act ?r)) s\"  
@@ -151,11 +191,5 @@ let genPart2 invNameList=
  next show \"∀ v va. v ∈ varOfSent (act ?r') ⟶va∈varOfExp ( substExpByStatement (IVar v)  (act ?r'))⟶ va ∈ (varOfFormList ?F)\"
       by auto  
   qed" str2 in
-   str1^end1
-
-let lemmaProof invNameList1 invNameList2 ruleName=
-    let head=lemmaHeadGen ruleName in
-    let part1= genPart1 invNameList1 in
-    let part2=genPart2 invNameList1 in
-    head^part1^part2
+   str1^end1*)
 
